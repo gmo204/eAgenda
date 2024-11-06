ec36221862b19ebe4734713de269447b1745c3fe
Gilberto Mota <gilbertojr204@gmail.com>
2024-11-06 14:01:39 -0300

    Lazy loading Aplicado e edicao da apiUrl

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,15 +1,7 @@
 import { CanMatchFn, Router, Routes, UrlTree } from '@angular/router';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { RegistroComponent } from './core/auth/views/registro/registro.component';
-import { LoginComponent } from './core/auth/views/login/login.component';
 import { UsuarioService } from './core/auth/services/usuario.service';
 import { inject } from '@angular/core';
 import { map, Observable } from 'rxjs';
-import { contatosRoutes } from './views/contatos/contatos.routes';
-import { compromissosRoutes } from './views/compromissos/compromissos.routes';
-import { tarefasRoutes } from './views/tarefas/tarefas.routes';
-import { categoriasRoutes } from './views/categorias/categorias.routes';
-import { despesasRoutes } from './views/despesas/depesas.routes';
 
 const authGuard: CanMatchFn = (): Observable<boolean | UrlTree> => {
   const router = inject(Router);
@@ -36,21 +28,68 @@ const authUserGuard: CanMatchFn = (): Observable<boolean | UrlTree> => {
   }));
 }
 
-
 export const routes: Routes = [
   {path: '', redirectTo:  'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent, canMatch: [authGuard]},
-
-  {path: 'registro', component: RegistroComponent, canMatch: [authUserGuard]},
-  {path: 'login', component: LoginComponent, canMatch: [authUserGuard]},
-
-  {path: 'contatos', children: contatosRoutes},
-
-  {path: 'compromissos', children: compromissosRoutes},
-
-  {path: 'categorias', children: categoriasRoutes},
-
-  {path: 'despesas', children: despesasRoutes},
-
-  {path: 'tarefas', children: tarefasRoutes}
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./views/dashboard/dashboard.component').then(
+      c => c.DashboardComponent),
+    canMatch: [authGuard]
+  },
+  {
+    path: 'registro',
+    loadComponent: () =>
+      import('./core/auth/views/registro/registro.component').then(
+        c => c.RegistroComponent
+      ),
+    canMatch: [authUserGuard]
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./core/auth/views/login/login.component').then(
+        c => c.LoginComponent
+      ),
+    canMatch: [authUserGuard]
+  },
+  {
+    path: 'contatos',
+    loadChildren: () =>
+      import('./views/contatos/contatos.routes').then(
+        (m) => m.contatosRoutes
+      ),
+    canMatch: [authGuard]
+  },
+  {
+    path: 'compromissos',
+    loadChildren: () =>
+      import('./views/compromissos/compromissos.routes').then(
+        (m) => m.compromissosRoutes
+      ),
+    canMatch: [authGuard]
+  },
+  {
+    path: 'categorias',
+    loadChildren: () =>
+      import('./views/categorias/categorias.routes').then(
+        (m) => m.categoriasRoutes
+      ),
+    canMatch: [authGuard]
+  },
+  {
+    path: 'despesas',
+    loadChildren: () =>
+      import('./views/despesas/depesas.routes').then(
+        (m) => m.despesasRoutes
+      ),
+    canMatch: [authGuard]
+  },
+  {
+    path: 'tarefas',
+    loadChildren: () =>
+      import('./views/tarefas/tarefas.routes').then(
+        (m) => m.tarefasRoutes
+      ),
+    canMatch: [authGuard]
+  }
 ];
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:4300/api'
+  apiUrl: 'https://e-agenda-api-bvx9.onrender.com/api',
 };
